2aab4e6a74a56d3f002cef951038d98f52d56847
Leonardo Nakao <leonardo@code49.net>
2020-08-16 20:26:13 -0300

    feat: organize store on modules

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import dashboard from './dashboard'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-    },
-    mutations: {
-    },
-    actions: {
-    },
     modules: {
+        dashboard
     }
 })
