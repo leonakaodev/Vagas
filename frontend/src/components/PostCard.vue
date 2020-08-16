199a0c028b218bd9523e62de8c0de935ae3edfa4
Leonardo Nakao <leonardo@code49.net>
2020-08-16 18:24:10 -0300

    feat: create PostCard

---
+++ frontend/src/components/PostCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card
+        class="mx-auto card-post"
+        min-width="400"
+    >
+        <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+            <v-card-title>{{ post.title }}</v-card-title>
+        </v-img>
+
+        <v-card-text class="text--primary">
+            <div>{{ post.description }}</div>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-btn
+                color="orange"
+                text
+            >
+                More +
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        post: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,41 @@
 <template>
-    <div class="home">
-        HOME
-        <i class="fa fa-home"></i>
-    </div>
+    <v-container>
+        <v-row>
+            <PostCard
+                v-for="(post, index) in posts"
+                :key="index"
+                :post="post"
+            />
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
+import PostCard from '@/components/PostCard'
 
 export default {
     name: 'Home',
+    components: {
+        PostCard
+    },
+    data: () => {
+        return {
+            posts: [
+                {title: 'Titulo', description: 'Descrição'},
+                {title: 'Titulo', description: 'Descrição'},
+                {title: 'Titulo', description: 'Descrição'},
+                {title: 'Titulo', description: 'Descrição'},
+                {title: 'Titulo', description: 'Descrição'},
+            ]
+        }
+    }
 }
 </script>
+
+
+<style scoped>
+.card-post {
+    width: 30vw;
+    margin-bottom: 30px;
+}
+</style>
