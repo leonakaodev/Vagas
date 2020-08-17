6a61e23ae99bded93576804c0a4186c6fc7c2a92
Leonardo Nakao <leonardo@code49.net>
2020-08-16 21:21:02 -0300

    chore: add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 const app = express()
 const { errors } = require('celebrate');
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errors())
