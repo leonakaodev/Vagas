63dc4618e1dee654cdeff671dd5682f3560c67ea
Leonardo Nakao <leonardo@code49.net>
2020-08-19 20:31:53 -0300

    refactor: create a form component to post

---
+++ frontend/src/components/posts/PostForm.vue
@@ -0,0 +1,114 @@
+<template>
+    <v-form ref="form" class="post-form">
+        <v-container>
+            <v-row>
+                <v-col cols="12" class="mb-5">
+                    <p class="mb-0">
+                        <a @click='generateImage()'>
+                            Generate random image url
+                        </a>
+                    </p>
+                    <v-text-field
+                        label="Image url"
+                        v-model="form.image"
+                        outlined
+                    ></v-text-field>
+                    <v-img
+                        v-if="form.image"
+                        :src="form.image"
+                        height="400"
+                    />
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        label="Title"
+                        v-model="form.title"
+                        outlined
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-textarea
+                        label="Description"
+                        v-model="form.description"
+                        outlined
+                    ></v-textarea>
+                </v-col>
+                <v-col cols="12">
+                    <p class="mb-0">
+                        <CreateCategory @create="newCategory" />
+                    </p>
+                    <v-select
+                        v-model="form.categories"
+                        :items="categoriesOptions"
+                        class="mt-0 pt-0"
+                        item-text="name"
+                        item-value="id"
+                        prepend-inner-icon="mdi-tag"
+                        multiple
+                        chips
+                    ></v-select>
+                </v-col>
+            </v-row>
+            <v-row justify="end" class="mt-4">
+                <v-col cols="3" align="right">
+                    <v-btn
+                        color="primary"
+                        @click="save"
+                    >
+                        Save
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import CreateCategory from '../categories/CreateCategory'
+
+export default {
+    data: () => {
+        return {
+            form: {
+                title: '',
+                description: '',
+                image: '',
+                categories: []
+            }
+        }
+    },
+    computed: {
+        ...mapState('categories', {
+            categoriesOptions: state => state.categories,
+        })
+    },
+    components: {
+        CreateCategory
+    },
+    methods: {
+        generateImage() {
+            this.form.image = 'https://unsplash.it/800/800'
+        },
+        newCategory(category) {
+            this.form.categories.push(category)
+        },
+        save(){
+            this.$emit('save', this.form)
+        },
+        ...mapActions('categories', {
+            loadCategories: 'load'
+        }),
+    },
+    created() {
+        this.loadCategories()
+    }
+}
+</script>
+
+<style scoped>
+.post-form .col{
+    padding-top: 0;
+    padding-bottom: 0;
+}
+</style>
--- frontend/src/views/CreatePost.vue
+++ frontend/src/views/CreatePost.vue
@@ -3,118 +3,27 @@
         <v-row class="d-flex justify-center align-items-center title">
             <h1>Create a new post</h1>
         </v-row>
-        <v-row>
-            <v-col cols="12">
-                <v-form ref="form">
-                    <v-row class="row-form">
-                        <v-col cols="12" class="mb-5">
-                            <p class="mb-0">
-                                <a @click='generateImage()'>
-                                    Generate random image url
-                                </a>
-                            </p>
-                            <v-text-field
-                                label="Image url"
-                                v-model="image"
-                                outlined
-                            ></v-text-field>
-                            <v-img
-                                v-if="image"
-                                :src="image"
-                                height="400"
-                            />
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field
-                                label="Title"
-                                v-model="title"
-                                outlined
-                            ></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                            <v-textarea
-                                label="Description"
-                                v-model="description"
-                                outlined
-                            ></v-textarea>
-                        </v-col>
-                        <v-col cols="12">
-                            <p class="mb-0">
-                                <CreateCategory @create="newCategory" />
-                            </p>
-                            <v-select
-                                v-model="categories"
-                                :items="categoriesOptions"
-                                class="mt-0 pt-0"
-                                item-text="name"
-                                item-value="id"
-                                prepend-inner-icon="mdi-tag"
-                                multiple
-                                chips
-                            ></v-select>
-                        </v-col>
-                    </v-row>
-                </v-form>
-            </v-col>
-            <v-col cols="12">
-                <v-row
-                    justify="end"
-                >
-                    <v-col
-                        cols="3"
-                        align="right"
-                    >
-                        <v-btn
-                            @click="create"
-                            color="primary"
-                        >
-                            Save
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
+        <PostForm @save="create" />
     </v-container>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import CreateCategory from '../components/categories/CreateCategory'
+import { mapActions } from 'vuex'
+import PostForm from '../components/posts/PostForm'
 
 export default {
     name: 'Home',
     data() {
         return {
-            title: '',
-            description: '',
-            image: undefined,
-            validImage: true,
-            categories: [],
         }
     },
     components: {
-        CreateCategory
-    },
-    computed: {
-        ...mapState('categories', {
-            categoriesOptions: state => state.categories,
-        })
+        PostForm
     },
     methods: {
-        generateImage() {
-            this.image = 'https://unsplash.it/800/800'
-        },
-        newCategory(category) {
-            this.categories.push(category)
-        },
-        async create(){
+        async create(form){
             try {
-                await this.createPost({
-                    title: this.title,
-                    description: this.description,
-                    image: this.validImage ? this.image : undefined,
-                    categories: this.categories
-                })
+                await this.createPost(form)
                 this.$store.commit('notifier/showMessage', {
                     content: 'Post created successfully',
                     type: 'success'
@@ -125,29 +34,15 @@ export default {
                 alert('An error occurred: ' + message)
             }
         },
-        addNewTag(){
-            alert('Option not allowed yet')
-        },
         ...mapActions('posts', {
             createPost: 'create'
         }),
-        ...mapActions('categories', {
-            loadCategories: 'load'
-        }),
     },
-    created() {
-        this.loadCategories()
-    }
 }
 </script>
 
 <style scoped>
 .title {
     padding: 50px 0;
-}
-
-.row-form .col{
-    padding-top: 0;
-    padding-bottom: 0;
 }
 </style>
