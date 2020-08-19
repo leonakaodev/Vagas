8a0339148ee6e1ec238f2ede932fc701b992bbe8
Leonardo Nakao <leonardo@code49.net>
2020-08-19 19:24:44 -0300

    feat: add Notfier component

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div id="app">
         <router-view/>
+        <Notifier />
     </div>
 </template>
 
 <script>
 import '../assets/css/main.css'
+import Notifier from './Notifier'
 
 export default {
-
+    components: {
+        Notifier
+    }
 }
 </script>
---
+++ frontend/src/components/Notifier.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-snackbar
+        v-model="show"
+        :timeout="timeout"
+        color="success"
+        bottom
+        right
+        text
+        dark
+    >
+        {{ message }}
+        <template v-slot:action="{ attrs }">
+            <v-btn
+                color="blue"
+                text
+                v-bind="attrs"
+                @click="show = false"
+            >
+                x
+            </v-btn>
+        </template>
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            show: true,
+            message: '',
+            type: '',
+            timeout: 2000
+        }
+    },
+
+    created () {
+        this.$store.subscribe((mutation, state) => {
+            if (mutation.type === 'notifier/showMessage') {
+                this.message = state.notifier.content
+                this.type = state.notifier.type
+                this.timeout = state.notifier.timeout
+                this.show = true
+            }
+        })
+    }
+}
+</script>
