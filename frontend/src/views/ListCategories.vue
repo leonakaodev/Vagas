399bff4ce983ef1142f189f11354a9b943cb4ae8
Leonardo Nakao <leonardo@code49.net>
2020-08-19 22:25:48 -0300

    feat: add button to create new categories

--- frontend/src/views/ListCategories.vue
+++ frontend/src/views/ListCategories.vue
@@ -4,18 +4,25 @@
             <h1>All categories</h1>
         </v-row>
         <v-row>
-            <CategoryList />
+            <v-col cols="12" align="right">
+                <CreateCategory />
+            </v-col>
+            <v-col cols="12">
+                <CategoryList />
+            </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
 import CategoryList from '@/components/categories/CategoryList'
+import CreateCategory from '../components/categories/CreateCategory'
 
 export default {
     name: 'ListCategories',
     components: {
-        CategoryList
+        CategoryList,
+        CreateCategory
     }
 }
 </script>
