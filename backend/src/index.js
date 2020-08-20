3909657046569f4cf095d4b0607280c0cf92c569
Leonardo Nakao <leonardo@code49.net>
2020-08-19 23:48:21 -0300

    chore: add dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,14 @@ const routes = require('./routes')
 const cors = require('cors')
 const app = express()
 const { errors } = require('celebrate');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errors())
 
-app.listen(3000, () => {
-  console.log('listening on http://localhost:3000')
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`listening on http://localhost:${process.env.SERVER_PORT}`)
 })
